de7abb95fedaffc03f14dcad6f6cf6e656a27b28
“PTorr” <torrpolakow@gmail.com>
2018-07-19 11:38:20 +0300

    1

--- read_data.py
+++ read_data.py
@@ -7,7 +7,7 @@ df = pd.read_csv('social_curiosity_6users.csv')
 df1 = df[df.columns[17:].tolist()]
 df1 = df1.drop(df.index[[0, 1]])
 df1 = df1.reset_index(drop = True)
-df1 = df1.rename(columns= {'Q1':'User1', 'Q2':'User2','Q3': 'Gender', 'Q4': 'Age', 'Q5': 'Education'})
+df1 = df1.rename(columns= {'Q1': 'User1', 'Q2': 'User2','Q3': 'Gender', 'Q4': 'Age', 'Q5': 'Education'})
 df2 = pd.DataFrame(np.zeros([len(df1), 15]), columns=['joyous_ex_self', 'deprivation_sens_self', 'stress_tol_self',
                                                     'social_cur_self', 'thrill_seek_self', 'joyous_ex_partner',
                                                     'deprivation_sens_partner', 'stress_tol_partner',
@@ -32,18 +32,50 @@ for i in range(5):
 
 for ind in df3.index:
     temp = df3[df3.User2 == df3.loc[ind, 'User1']].iloc[:, -10:-5]
-    df3.iloc[ind,-5:] = np.asarray(temp)
+    df3.iloc[ind, -5:] = np.asarray(temp)
 
-df3.drop(['joyous_ex_partner','deprivation_sens_partner', 'stress_tol_partner','social_cur_partner', 'thrill_seek_partner'], axis=1)
-
+df3.drop(['joyous_ex_partner', 'deprivation_sens_partner', 'stress_tol_partner', 'social_cur_partner', 'thrill_seek_partner'], axis=1)
 averages = pd.Series(index = df3.columns, data = np.zeros(len(df3.columns)))
 for i in range(len(df3.columns)):
     averages[i] = np.mean(df3.iloc[:, i])
-
+array_two = [0,1]
 df3.to_csv('clear_data_frame.csv')
 df1.to_csv('df1_data_frame')
 print (df3)
-pd.df3.plot
+
+
+comparison = pd.DataFrame({'self': list(averages[55:60]),
+                               'partner': list(averages[60:65])},
+                              index=['joyous_ex', 'deprivation_sens', 'stress_tol', 'social_cur', 'thrill_seek'])
+
+def graph_individual():
+
+    joyous_ax = df3[df3.columns[df3.columns.str.contains('joyous_ex')][array_two]].plot.bar()
+    depr_ax = df3[df3.columns[df3.columns.str.contains('deprivation_sens')][array_two]].plot.bar()
+    stre_ax = df3[df3.columns[df3.columns.str.contains('stress_tol')][array_two]].plot.bar()
+    socia_ax = df3[df3.columns[df3.columns.str.contains('social_cur')][array_two]].plot.bar()
+    thri_ax = df3[df3.columns[df3.columns.str.contains('thrill_seek')][array_two]].plot.bar()
+
+    joyous_ax.set_title('joyous_ex')
+    depr_ax.set_title('deprivation_sens')
+    stre_ax.set_title('stress_tol')
+    socia_ax.set_title('social_cur')
+    thri_ax.set_title('thrill_seek')
+
+def graph_type():
+    comp_ax = comparison.plot.bar()
+    comp_ax.set_title('all_types')
+
+def graph_all():
+    all_ax = np.mean(comparison).plot.bar()
+    all_ax.set_title('combined')
+
+
+#graph_type()
+#graph_individual()
+#graph_all()
+plt.show()
+
 
 
 from IPython import embed
